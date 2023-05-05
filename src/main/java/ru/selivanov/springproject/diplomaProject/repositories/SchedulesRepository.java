b7f1739475012a9293806165cfa7f7d6c2100c65
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-05 18:13:01 +0300

    Add findByAudience and findByDayOfWeek methods

--- src/main/java/ru/selivanov/springproject/diplomaProject/repositories/SchedulesRepository.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/repositories/SchedulesRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.selivanov.springproject.diplomaProject.model.Schedule;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface SchedulesRepository extends JpaRepository<Schedule, Integer> {
+    List<Schedule> findByAudienceAndDayOfWeekAndStartTime(String audience, String dayOfWeek, Date startTime);
+    List<Schedule> findByAudience(String audience);
+    List<Schedule> findByDayOfWeek(String dayOfWeek);
+
 }
