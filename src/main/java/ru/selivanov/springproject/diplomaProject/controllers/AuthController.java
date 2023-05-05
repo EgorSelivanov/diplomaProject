392a7670fb50a3f424d9d28e4c0c0bbfd00be2fc
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-05 18:10:38 +0300

    Add changePassword GET and POST requests

--- src/main/java/ru/selivanov/springproject/diplomaProject/controllers/AuthController.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/controllers/AuthController.java
@@ -4,23 +4,24 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.selivanov.springproject.diplomaProject.dto.PasswordDTO;
 import ru.selivanov.springproject.diplomaProject.model.User;
 import ru.selivanov.springproject.diplomaProject.services.RegistrationService;
+import ru.selivanov.springproject.diplomaProject.util.PasswordValidator;
 import ru.selivanov.springproject.diplomaProject.util.UserValidator;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
     private final UserValidator userValidator;
+    private final PasswordValidator passwordValidator;
     private final RegistrationService registrationService;
 
     @Autowired
-    public AuthController(UserValidator userValidator, RegistrationService registrationService) {
+    public AuthController(UserValidator userValidator, PasswordValidator passwordValidator, RegistrationService registrationService) {
         this.userValidator = userValidator;
+        this.passwordValidator = passwordValidator;
         this.registrationService = registrationService;
     }
 
@@ -37,6 +38,9 @@ public class AuthController {
     @PostMapping("/registration")
     public String performRegistration(@ModelAttribute("user") @Valid User user,
                                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "/auth/registration";
+
         userValidator.validate(user, bindingResult);
 
         if (bindingResult.hasErrors())
@@ -45,5 +49,25 @@ public class AuthController {
         registrationService.register(user);
 
         return "redirect:/auth/login";
+    }
+
+    @GetMapping("/change-password")
+    public String changePasswordPage(@ModelAttribute("passwordDTO")PasswordDTO passwordDTO) {
+        return "auth/modalChangePassword";
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(@ModelAttribute("passwordDTO") @Valid PasswordDTO passwordDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "/auth/modalChangePassword";
+
+        passwordValidator.validate(passwordDTO, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "/auth/modalChangePassword";
+
+        registrationService.changePassword(passwordDTO.getNewPassword());
+
+        return "redirect:/student/1";
     }
 }
