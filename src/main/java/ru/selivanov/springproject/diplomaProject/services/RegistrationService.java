54aba270078ae320c0aae094837b394d8ded3f6f
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-05 18:09:43 +0300

    Add changePassword method

--- src/main/java/ru/selivanov/springproject/diplomaProject/services/RegistrationService.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/services/RegistrationService.java
@@ -2,10 +2,13 @@ package ru.selivanov.springproject.diplomaProject.services;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.selivanov.springproject.diplomaProject.model.User;
 import ru.selivanov.springproject.diplomaProject.repositories.UsersRepository;
+import ru.selivanov.springproject.diplomaProject.security.UserDetails;
 
 @Service
 public class RegistrationService {
@@ -21,6 +24,16 @@ public class RegistrationService {
     @Transactional
     public void register(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        usersRepository.save(user);
+    }
+
+    @Transactional
+    public void changePassword(String password) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userDetails.getUser();
+        user.setPassword(passwordEncoder.encode(password));
 
         usersRepository.save(user);
     }
