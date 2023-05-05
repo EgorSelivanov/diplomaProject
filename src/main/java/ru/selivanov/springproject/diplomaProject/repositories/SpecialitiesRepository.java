a19ec68255237745bfd2e72359cd876dc8c2442f
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-05 18:12:25 +0300

    Add findByName and findByCode methods

--- src/main/java/ru/selivanov/springproject/diplomaProject/repositories/SpecialitiesRepository.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/repositories/SpecialitiesRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.selivanov.springproject.diplomaProject.model.Speciality;
 
+import java.util.Optional;
+
 @Repository
 public interface SpecialitiesRepository extends JpaRepository<Speciality, Integer> {
+    Optional<Speciality> findByNameLike(String name);
+    Optional<Speciality> findByCodeLike(String code);
 }
