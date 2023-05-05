3604141cfe88f30ecd2b20d66dad24ec367f4c67
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-05 18:11:43 +0300

    Add findByType method

--- src/main/java/ru/selivanov/springproject/diplomaProject/repositories/WorkloadsRepository.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/repositories/WorkloadsRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.selivanov.springproject.diplomaProject.model.Workload;
 
+import java.util.List;
+
 @Repository
 public interface WorkloadsRepository extends JpaRepository<Workload, Integer> {
+    List<Workload> findByTypeLike(String type);
+
+
 }
