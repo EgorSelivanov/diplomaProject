e7ce7dbfd87174c5ca5e2facaed32526bf3a71ba
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-13 16:49:26 +0300

    Add a redirect depending on the type of user

--- src/main/java/ru/selivanov/springproject/diplomaProject/controllers/PathController.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/controllers/PathController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController {
+public class PathController {
     @GetMapping("/error")
     public String getErrorPage() {
         return "error";
     }
+
+    @GetMapping("/")
+    public String getRedirect() {
+        return "redirect:/auth/redirect";
+    }
 }
+
