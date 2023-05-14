4d4b28f80d219318d590e6a933973985c7624408
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-14 18:09:47 +0300

    Add dto for displaying attendances of students

---
+++ src/main/java/ru/selivanov/springproject/diplomaProject/dto/AttendanceOfStudentsDTO.java
@@ -0,0 +1,86 @@
+package ru.selivanov.springproject.diplomaProject.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AttendanceOfStudentsDTO {
+    private final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+    @NotEmpty(message = "Имя пользователя не может быть пустым!")
+    @Size(min = 2, max = 255, message = "Имя пользователя должно быть от 2 до 255 символов!")
+    private String firstName;
+
+    @NotEmpty(message = "Фамилия пользователя не может быть пустой!")
+    @Size(min = 2, max = 255, message = "Фамилия пользователя должна быть от 2 до 255 символов!")
+    private String secondName;
+
+    private String patronymic;
+
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    private Date date;
+
+    @Min(0)
+    private int present;
+
+    public AttendanceOfStudentsDTO() {}
+
+    public AttendanceOfStudentsDTO(String firstName, String secondName, String patronymic, Date date, int present) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.patronymic = patronymic;
+        this.date = date;
+        this.present = present;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPresent() {
+        return present;
+    }
+
+    public void setPresent(int present) {
+        this.present = present;
+    }
+
+    public String getDateFormat() {
+        return formatter.format(date);
+    }
+
+    public String getFullName() {
+        return this.secondName + " " + this.firstName + " " + this.patronymic;
+    }
+}
---
+++ src/main/java/ru/selivanov/springproject/diplomaProject/dto/AttendanceToShowDTO.java
@@ -0,0 +1,62 @@
+package ru.selivanov.springproject.diplomaProject.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendanceToShowDTO {
+
+    @NotEmpty(message = "ФИО пользователя не может быть пустым!")
+    @Size(min = 2, max = 255, message = "Имя пользователя должно быть от 2 до 255 символов!")
+    private String fullName;
+
+    private List<String> dateList;
+
+    private List<Integer> presentList;
+
+    public AttendanceToShowDTO() {
+        this.dateList = new ArrayList<>();
+        this.presentList = new ArrayList<>();
+    }
+
+    public AttendanceToShowDTO(String fullName, List<String> dateList, List<Integer> presentList) {
+        this.fullName = fullName;
+        this.dateList = dateList;
+        this.presentList = presentList;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public List<String> getDateList() {
+        return dateList;
+    }
+
+    public void setDateList(List<String> dateList) {
+        this.dateList = dateList;
+    }
+
+    public List<Integer> getPresentList() {
+        return presentList;
+    }
+
+    public void setPresentList(List<Integer> presentList) {
+        this.presentList = presentList;
+    }
+
+    public void addDate(String date) {
+        this.dateList.add(date);
+    }
+
+    public void addPresent(Integer present) {
+        this.presentList.add(present);
+    }
+}
