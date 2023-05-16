d7e138ad3615b8d6c9167c74404a4dd05ca2394c
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-17 00:30:28 +0300

    Add getting grades by student and assignment data

--- src/main/java/ru/selivanov/springproject/diplomaProject/repositories/GradesRepository.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/repositories/GradesRepository.java
@@ -2,8 +2,13 @@ package ru.selivanov.springproject.diplomaProject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.selivanov.springproject.diplomaProject.model.Assignment;
 import ru.selivanov.springproject.diplomaProject.model.Grade;
+import ru.selivanov.springproject.diplomaProject.model.Student;
+
+import java.util.Optional;
 
 @Repository
 public interface GradesRepository extends JpaRepository<Grade, Integer> {
+    Optional<Grade> findByStudentAndAssignment(Student student, Assignment assignment);
 }
--- src/main/java/ru/selivanov/springproject/diplomaProject/services/GradeService.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/services/GradeService.java
@@ -6,7 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.selivanov.springproject.diplomaProject.model.Assignment;
 import ru.selivanov.springproject.diplomaProject.model.Grade;
 import ru.selivanov.springproject.diplomaProject.model.Student;
+import ru.selivanov.springproject.diplomaProject.repositories.AssignmentsRepository;
 import ru.selivanov.springproject.diplomaProject.repositories.GradesRepository;
+import ru.selivanov.springproject.diplomaProject.repositories.StudentsRepository;
 
 import java.util.Optional;
 
@@ -14,10 +16,14 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class GradeService {
     private final GradesRepository gradesRepository;
+    private final StudentsRepository studentsRepository;
+    private final AssignmentsRepository assignmentsRepository;
 
     @Autowired
-    public GradeService(GradesRepository gradesRepository) {
+    public GradeService(GradesRepository gradesRepository, StudentsRepository studentsRepository, AssignmentsRepository assignmentsRepository) {
         this.gradesRepository = gradesRepository;
+        this.studentsRepository = studentsRepository;
+        this.assignmentsRepository = assignmentsRepository;
     }
 
     @Transactional
@@ -36,6 +42,10 @@ public class GradeService {
         return true;
     }
 
+    @Transactional
+    public void saveGrade(Grade grade) {
+        gradesRepository.save(grade);
+    }
     @Transactional
     public void deleteGrade(int id) {
         gradesRepository.deleteById(id);
@@ -65,5 +75,14 @@ public class GradeService {
                     grade.setAssignment(selectedAssignment);
                 }
         );
+    }
+
+    public Grade getGradeByStudentAndAssignment(int studentId, int assignmentId) {
+        Student student = studentsRepository.findById(studentId).orElse(null);
+        Assignment assignment = assignmentsRepository.findById(assignmentId).orElse(null);
+        if (student == null || assignment == null)
+            return null;
+
+        return gradesRepository.findByStudentAndAssignment(student, assignment).orElse(null);
     }
 }
