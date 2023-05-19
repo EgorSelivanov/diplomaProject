e5dfc4ae796488437e6ae46ba03121dd93e7d0bc
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-19 15:05:56 +0300

    Add a personal area page

---
+++ src/main/resources/static/js/personal.js
@@ -0,0 +1,38 @@
+function confirmDeletion(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке по умолчанию
+
+    var url = event.currentTarget.getAttribute('href');
+
+    customConfirm('Вы уверены, что хотите удалить администратора?');
+    var modal = document.getElementById('modal-div');
+    const csrfToken = document.getElementById("csrfToken").value;
+    // Обработчики клика по кнопкам в окне подтверждения
+    document.querySelector(".confirmModalYes").addEventListener("click", function() {
+        // Закрыть окно подтверждения
+        modal.innerHTML = '';
+        console.log(url);
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+            }
+        })
+            .then(response => {
+                return response.text();
+            })
+            .then(data => {
+                customAlert(data);
+                window.location.href = `/admin/${adminId}`;
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                customAlert(error.message);
+            });
+    });
+
+    document.querySelector(".confirmModalNo").addEventListener("click", function() {
+        // Закрыть окно подтверждения
+        modal.innerHTML = '';
+    });
+}
