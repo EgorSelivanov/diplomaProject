780fbd9ce5e19884d13d4f0d5a8495fe057c270d
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-19 15:02:22 +0300

    Add methods to get an adminList

--- src/main/java/ru/selivanov/springproject/diplomaProject/repositories/UsersRepository.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/repositories/UsersRepository.java
@@ -12,4 +12,10 @@ public interface UsersRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     List<User> findByFirstNameLikeOrSecondNameLike(String firstName, String secondName);
+    List<User> findByRole(String role);
+    List<User> findByRoleAndUsernameLikeIgnoreCase(String role, String username);
+    List<User> findByRoleAndEmailLikeIgnoreCase(String role, String email);
+    List<User> findByRoleAndFirstNameLikeIgnoreCase(String role, String firstName);
+    List<User> findByRoleAndSecondNameLikeIgnoreCase(String role, String secondName);
+    List<User> findByRoleAndPatronymicLikeIgnoreCase(String role, String patronymic);
 }
--- src/main/java/ru/selivanov/springproject/diplomaProject/services/AdminService.java
+++ src/main/java/ru/selivanov/springproject/diplomaProject/services/AdminService.java
@@ -1,9 +1,38 @@
 package ru.selivanov.springproject.diplomaProject.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.selivanov.springproject.diplomaProject.model.User;
+import ru.selivanov.springproject.diplomaProject.repositories.UsersRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdminService {
+    private final UsersRepository usersRepository;
 
+    @Autowired
+    public AdminService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
+    public User getUserById(int id) {
+        return usersRepository.findById(id).orElse(null);
+    }
+
+    public List<User> getAdminList() {
+        return usersRepository.findByRole("ROLE_ADMIN");
+    }
+
+    public List<User> getAdminListSearch(String search) {
+        List<User> list = new ArrayList<>();
+        search = "%" + search + "%";
+        list.addAll(usersRepository.findByRoleAndUsernameLikeIgnoreCase("ROLE_ADMIN", search));
+        list.addAll(usersRepository.findByRoleAndEmailLikeIgnoreCase("ROLE_ADMIN", search));
+        list.addAll(usersRepository.findByRoleAndFirstNameLikeIgnoreCase("ROLE_ADMIN", search));
+        list.addAll(usersRepository.findByRoleAndSecondNameLikeIgnoreCase("ROLE_ADMIN", search));
+        list.addAll(usersRepository.findByRoleAndPatronymicLikeIgnoreCase("ROLE_ADMIN", search));
+        return list;
+    }
 }
