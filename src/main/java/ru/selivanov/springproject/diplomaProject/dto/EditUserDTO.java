6924f4264d92f25859a36f1e16aa7f90623c05fd
Egor Selivanov <egorselivanov0@gmail.com>
2023-05-19 15:20:39 +0300

    Add an editUserDTO

---
+++ src/main/java/ru/selivanov/springproject/diplomaProject/dto/EditUserDTO.java
@@ -0,0 +1,135 @@
+package ru.selivanov.springproject.diplomaProject.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import ru.selivanov.springproject.diplomaProject.model.User;
+
+public class EditUserDTO {
+    private int userId;
+
+    private int adminId;
+
+    @NotEmpty(message = "Имя пользователя не может быть пустым!")
+    @Size(min = 2, max = 255, message = "Имя пользователя должно быть от 2 до 255 символов!")
+    private String username;
+
+    @Email
+    @NotEmpty(message = "Данные почты не могут быть пустыми!")
+    private String email;
+
+    @NotEmpty(message = "Не выбрана роль пользователя")
+    private String role;
+
+    private String password;
+
+    @NotEmpty(message = "Имя пользователя не может быть пустым!")
+    @Size(min = 2, max = 255, message = "Имя пользователя должно быть от 2 до 255 символов!")
+    private String firstName;
+
+    @NotEmpty(message = "Фамилия пользователя не может быть пустой!")
+    @Size(min = 2, max = 255, message = "Фамилия пользователя должна быть от 2 до 255 символов!")
+    private String secondName;
+
+    private String patronymic;
+
+    public EditUserDTO() {}
+
+    public EditUserDTO(User user, int adminId) {
+        this.userId = user.getUserId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+        this.firstName = user.getFirstName();
+        this.secondName = user.getSecondName();
+        this.patronymic = user.getPatronymic();
+        this.adminId = adminId;
+    }
+
+    public EditUserDTO(int userId, int adminId, String username, String password, String email, String role, String firstName, String secondName, String patronymic) {
+        this.userId = userId;
+        this.adminId = adminId;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.patronymic = patronymic;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public User getUser() {
+        return new User(username, email, role, firstName, secondName, patronymic);
+    }
+}
